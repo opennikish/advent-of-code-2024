94da4756ea6fbe621dff1cc04263b4034ea137d0
Nikita V <nikita.postpunk@gmail.com>
2024-12-10 20:54:40 +0100

    Optimize day 9 part 2

--- 09/2.go
+++ 09/2.go
@@ -4,11 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"slices"
 )
 
 type slot struct {
-	i, size int
+	i, size, id int
 }
 
 func main() {
@@ -16,113 +15,74 @@ func main() {
 	fmt.Println(res)
 }
 
-func print(fmap []int) {
-	for _, x := range fmap {
-		if x >= 0 {
-			fmt.Printf("%d", x)
-		} else {
-			fmt.Print(".")
-		}
-	}
-	fmt.Println()
-}
-
 func solve(path string) int {
 	bs, err := os.ReadFile(path)
 	check(err)
-	encodedFileMap := bytes.TrimSpace(bs)
+	fmap := bytes.TrimSpace(bs)
 
-	fmap := convert(encodedFileMap)
-	// print(fmap)
+	fileSlots, emptySlots := collect(fmap)
 
-	emptySlots := filterEmptySlots(fmap)
+	for i := len(fileSlots) - 1; i >= 0; i-- {
+		file := fileSlots[i]
 
-	for i := len(fmap) - 1; i >= 0; {
-		if fmap[i] == -1 {
-			i--
-			continue
-		}
+		for j, empty := range emptySlots {
+			if empty.i > file.i {
+				emptySlots = emptySlots[0:j]
+				break
+			}
 
-		end, id := i, fmap[i]
-		for i >= 0 && fmap[i] == id {
-			i--
-		}
-
-		length := end - i
-		for _, slot := range emptySlots {
-			if slot.size < length || slot.i > i+1 {
+			if empty.size < file.size {
 				continue
 			}
 
-			copy(fmap[slot.i:slot.i+length], fmap[i+1:end+1])
-			for j := i + 1; j <= end; j++ {
-				fmap[j] = -1
-			}
+			pos := empty.i
+			empty.i += file.size
+			empty.size -= file.size
+			file.i = pos
 
-			slot.size -= length
-			slot.i += length
+			break
+		}
 
+		if len(emptySlots) == 0 {
 			break
 		}
 	}
 
-	// print(fmap)
-
-	return checksum(fmap)
+	return checksum(fileSlots)
 }
 
-// [0,0,-1,-1,-1,-1,2,2,2,-1,-1]
-func filterEmptySlots(fmap []int) []*slot {
-	res := []*slot{}
-
-	for i := 0; i < len(fmap); {
-		x := fmap[i]
-		if x != -1 {
-			i++
-			continue
-		}
-
-		start := i
-		for i < len(fmap) && fmap[i] == -1 {
-			i++
-		}
-		res = append(res, &slot{i: start, size: i - start})
-	}
-
-	return res
-}
-
-func checksum(fmap []int) int {
+func checksum(fileSlots []*slot) int {
 	s := 0
-	for i, id := range fmap {
-		if id == -1 {
-			continue
+	for _, file := range fileSlots {
+		for k := file.i; k < file.i+file.size; k++ {
+			s += file.id * k
 		}
-		s += id * i
 	}
 	return s
 }
 
-// 12345
-// 0..111....22222
-func convert(fmap []byte) []int {
-	res := []int{}
-	id := 0
+func collect(fmap []byte) ([]*slot, []*slot) {
+	fileSlots, emptySlots := []*slot{}, []*slot{}
+	id, pos := 0, 0
 	for i, b := range fmap {
-		d := int(b - '0')
-
-		var next int
+		size := int(b - '0')
 		if (i+1)%2 == 0 {
-			next = -1
+			emptySlots = append(emptySlots, &slot{i: pos, size: size})
 		} else {
-			next = id
+			fileSlots = append(fileSlots, &slot{i: pos, size: size, id: id})
 			id++
 		}
-
-		res = append(res, slices.Repeat([]int{next}, d)...)
+		pos += size
 	}
 
-	return res
+	return fileSlots, emptySlots
+}
+
+func print(fileSlots []*slot) {
+	for _, s := range fileSlots {
+		fmt.Printf("(%d, %d, %d) ", s.i, s.size, s.id)
+	}
+	fmt.Println()
 }
 
 func check(err error) {
